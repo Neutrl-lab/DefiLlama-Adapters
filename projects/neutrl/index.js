da982f5f2f217dfd59e049f2f14109febe8d0a4e
Kuchi <kuchi@neutrl.xyz>
2025-12-29 11:47:58 +0100

    Typo

--- projects/neutrl/index.js
+++ projects/neutrl/index.js
@@ -2,7 +2,7 @@ const NUSD = "0xE556ABa6fe6036275Ec1f87eda296BE72C811BCE";
 
 module.exports = {
     start: 23495853,
-    methodology: `TVL is calculated by fetching the total supply of the NUSD stablecoin contract on Ethereum.`,
+    methodology: "TVL is calculated by fetching the total supply of the NUSD stablecoin contract on Ethereum.",
     ethereum: {
         tvl: async (api) => {
             const supply = await api.call({ abi: 'erc20:totalSupply', target: NUSD });
